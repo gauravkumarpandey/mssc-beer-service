4d9fc86fd61567a98ca3731c8fe8b1b89152f66c
Gaurav Pandey <gauravkumar.pandey@dxc.com>
2024-09-24 12:03:20 +0530

    Project setup

---
+++ src/main/java/com/springguru/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,14 @@
+package com.springguru.msscbeerservice.web.model;
+
+public enum BeerStyle {
+    LAGER, 
+    PILSNER, 
+    STOUT, 
+    GOSE, 
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON 
+}
